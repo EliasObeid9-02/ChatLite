adf95dbca0c62e8d1a53dea91872e36ca3a40f70
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-25 16:48:56 +0300

    feat(chats): added URLs for channels

---
+++ chats/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from chats.views import (
+    ChannelView,
+    CreateChannelView,
+    JoinChannelView,
+    RegenerateInviteCodeView,
+)
+
+app_name = "chats"
+
+urlpatterns = [
+    path("channel/create/", CreateChannelView.as_view(), name="create-channel"),
+    path("channel/<uuid:channel_id>/", ChannelView.as_view(), name="channel-view"),
+    path("join/<uuid:invite_code>/", JoinChannelView.as_view(), name="join-channel"),
+    path(
+        "channel/<uuid:channel_id>/regenerate-invite/",
+        RegenerateInviteCodeView.as_view(),
+        name="regenerate-invite-code",
+    ),
+]
