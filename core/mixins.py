cb16193a7fe56ec0e939f8981e3278bacbe3d00c
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-26 16:52:42 +0300

    feat(core): added method that returns true if request is HTMX

--- core/mixins.py
+++ core/mixins.py
@@ -6,9 +6,12 @@ class HtmxMixin:
     Mixin to add HTMX-specific context variables to a view.
     """
 
+    def is_htmx_request(self):
+        return self.request.headers.get("HX-Request") == "true"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["is_htmx_request"] = self.request.headers.get("HX-Request") == "true"
+        context["is_htmx_request"] = self.is_htmx_request()
         return context
 
 
