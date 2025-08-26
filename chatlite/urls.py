c62b4e2c3909c446269126390ef1e90873b56dc2
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-26 16:54:57 +0300

    feat(users): updated URLs to use the new views correctly

--- chatlite/urls.py
+++ chatlite/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
+
+from users.views import HomeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("users.urls", namespace="users")),
     path("", include("chats.urls", namespace="chats")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", HomeView.as_view(), name="home"),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,12 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from users.views import ProfileView, RegisterView
+from users.views import LoginView, LogoutView, ProfileView, RegisterView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="users/login.html"),
-        name="login",
-    ),
-    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
-    path("profile/", ProfileView.as_view(), name="users-profile"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("profile/<uuid:user_id>/", ProfileView.as_view(), name="profile"),
 ]
