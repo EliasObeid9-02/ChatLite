3c7602a80fbf9004f0cb873e7079f15859565b32
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-29 13:28:18 +0300

    feat(chats): implemented ChannelCreateFrom

--- chats/forms.py
+++ chats/forms.py
@@ -3,26 +3,37 @@ from django import forms
 from chats.models import Channel
 
 
-class ChannelCreationForm(forms.ModelForm):
-    """
-    A form for creating a new Channel.
-    """
+class ChannelCreateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user")
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if self.user:
+            instance.owner = self.user
+        if commit:
+            instance.save()
+        return instance
 
     class Meta:
         model = Channel
         fields = ("name", "description")
         widgets = {
-            "name": forms.TextInput(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter a channel name",
-                }
-            ),
+            "name": forms.TextInput(attrs={"placeholder": "Channel Name"}),
             "description": forms.Textarea(
-                attrs={
-                    "class": "form-control",
-                    "placeholder": "Enter an optional description for the channel",
-                    "rows": 4,
-                }
+                attrs={"placeholder": "Channel Description (optional)", "rows": 3}
+            ),
+        }
+
+
+class ChannelUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Channel
+        fields = ("name", "description")
+        widgets = {
+            "name": forms.TextInput(attrs={"placeholder": "New Channel Name"}),
+            "description": forms.Textarea(
+                attrs={"placeholder": "New Channel Description (optional)", "rows": 3}
             ),
         }
