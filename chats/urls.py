8becac48b11e9b56d53610ab91dfcc13b97d79ec
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-29 18:24:30 +0300

    refactor(chats): setup URLs for chats application, includes create, details/update, chat view, join channel, and generate invite link

--- chats/urls.py
+++ chats/urls.py
@@ -1,23 +1,33 @@
 from django.urls import path
 
 from chats.views import (
+    ChannelChatView,
     ChannelView,
     CreateChannelView,
+    GenerateInviteCodeView,
     HomeView,
     JoinChannelView,
-    RegenerateInviteCodeView,
 )
 
 app_name = "chats"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("channel/create/", CreateChannelView.as_view(), name="create-channel"),
-    path("channel/<uuid:channel_id>/", ChannelView.as_view(), name="channel-view"),
-    path("join/<uuid:invite_code>/", JoinChannelView.as_view(), name="join-channel"),
+    path("channel/create/", CreateChannelView.as_view(), name="channel-create"),
+    path("channel/<uuid:channel_id>/", ChannelChatView.as_view(), name="channel-chat"),
     path(
-        "channel/<uuid:channel_id>/regenerate-invite/",
-        RegenerateInviteCodeView.as_view(),
-        name="regenerate-invite-code",
+        "channel/<uuid:channel_id>/details/",
+        ChannelView.as_view(),
+        name="channel-details",
+    ),
+    path(
+        "channel/join/<uuid:invite_code>/",
+        JoinChannelView.as_view(),
+        name="channel-join",
+    ),
+    path(
+        "channel/<uuid:channel_id>/generate-invite/",
+        GenerateInviteCodeView.as_view(),
+        name="generate-invite",
     ),
 ]
