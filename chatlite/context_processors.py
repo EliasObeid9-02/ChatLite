51ba0c3b48febb8959b26dba7cf856ccdf92e71d
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-29 18:23:34 +0300

    feat: added a context processor for sidebar rendering

--- chatlite/context_processors.py
+++ chatlite/context_processors.py
@@ -1,5 +1,17 @@
 import re
 
+
+def sidebar_context(request):
+    context = {}
+    if request.user.is_authenticated:
+        context["channels"] = request.user.member_of.all()
+
+        # Extract active channel ID from the request path
+        match = re.search(r"channel/([0-9a-f-]+)/", request.path)
+        active_channel_id = match.group(1) if match else None
+        context["active_channel_id"] = active_channel_id
+    return context
+
+
 def htmx_context(request):
     return {"is_htmx_request": request.headers.get("HX-Request") == "true"}
-
