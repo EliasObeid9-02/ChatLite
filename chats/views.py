6af645944be9034164ab663476c4714905c12e15
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-29 18:24:56 +0300

    refactor(chats): refactored all views for chats application, channel chat view to be implemented

--- chats/views.py
+++ chats/views.py
@@ -1,80 +1,95 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseForbidden
-from django.shortcuts import redirect
-from django.views.generic import CreateView, DetailView, RedirectView, TemplateView
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView, View
 
-from chats.models import Channel, ChannelMember
-from core.mixins import HtmxMixin
+from chats.forms import ChannelCreateForm, ChannelUpdateForm
+from chats.models import Channel
 
 
-class ChannelContextMixin:
-    """A mixin that adds the list of user's channels to the context."""
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = "chats/home.html"
+
+
+# TODO: implement
+class ChannelChatView(LoginRequiredMixin, View):
+    template_name = "chats/channel_chat.html"
+
+    def get(self, request, channel_id):
+        return HttpResponse(f"Channel View for ID: {channel_id}")
+
+
+class CreateChannelView(LoginRequiredMixin, CreateView):
+    template_name = "chats/channel_create.html"
+    form_class = ChannelCreateForm
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        self.object = form.save()
+        response = redirect(self.object.get_absolute_url())
+        response["HX-Trigger"] = "channelCreated"
+        return response
+
+
+class ChannelView(LoginRequiredMixin, TemplateView):
+    template_name = "chats/channel_view.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if self.request.user.is_authenticated:
-            context["user_channels"] = ChannelMember.objects.filter(
-                user=self.request.user
-            ).select_related("channel")
+        channel_id = self.kwargs.get("channel_id")
+
+        channel = get_object_or_404(Channel, id=channel_id)
+        context["channel"] = channel
+        context["is_owner"] = channel.owner == self.request.user
+
+        if self.request.user == channel.owner:
+            context["form"] = ChannelUpdateForm(instance=channel)
         return context
 
+    def post(self, request, *args, **kwargs):
+        channel_id = self.kwargs.get("channel_id")
+        channel = get_object_or_404(Channel, id=channel_id)
 
-class HomeView(HtmxMixin, ChannelContextMixin, TemplateView):
-    """
-    Acts as a router based on authentication status.
-    - Authenticated users see the main application.
-    - Unauthenticated users are redirected to the login page.
-    """
+        if request.user != channel.owner:
+            return render(request, "unauthorized.html")
 
-    template_name = "chats/home.html"
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            return redirect("users:login")
-        return super().dispatch(request, *args, **kwargs)
-
-
-        )
-
-        # Automatically add the creator as a member
-        ChannelMember.objects.create(user=request.user, channel=channel)
-        return redirect("channel_view", channel_id=channel.id)
+        form = ChannelUpdateForm(request.POST, instance=channel)
+        if form.is_valid():
+            channel.save()
+            response = redirect("chats:channel-details", channel_id=channel_id)
+            response["HX-Trigger"] = "channelUpdated"
+            return response
+        context = self.get_context_data()
+        context["form"] = form
+        return render(request, self.template_name, context)
 
 
 class JoinChannelView(LoginRequiredMixin, View):
-    """View to handle a user joining a channel via invite link."""
+    def get(self, request, invite_code):
+        try:
+            channel = Channel.objects.get(invite_code=invite_code)
+        except Channel.DoesNotExist:
+            return render(request, "chats/invalid_invite.html", status=404)
 
-    def get(self, request, invite_code, *args, **kwargs):
-        # Find the channel using the invite_code
-        # Because every invite code is unique to a single channel
-        # when an invite code is overridden it will not work anymore
-        channel = get_object_or_404(Channel, invite_code=invite_code)
-        ChannelMember.objects.get_or_create(user=request.user, channel=channel)
-        return redirect("channel_view", channel_id=channel.id)
+        user = request.user
+        # if the user is not a member we add them and redirect to the chat
+        if user not in channel.members.all():
+            channel.members.add(user)
+        return redirect(channel.get_absolute_url())
 
 
-class RegenerateInviteCodeView(LoginRequiredMixin, View):
-    """Allows the channel creator to regenerate the invite code."""
-
-    def post(self, request, channel_id, *args, **kwargs):
+class GenerateInviteCodeView(LoginRequiredMixin, View):
+    def post(self, request, channel_id):
         channel = get_object_or_404(Channel, id=channel_id)
 
-        # Security check: Only the channel creator can regenerate the link
-        if channel.creator != request.user:
-            return HttpResponseForbidden(
-                "You are not authorized to perform this action."
-            )
+        if request.user != channel.owner:
+            return render(request, "unauthorized.html")
 
-        channel.regenerate_invite_code()
-        return redirect("channel_view", channel_id=channel.id)
-
-
-class ChannelView(LoginRequiredMixin, View):
-    """View to display a channel's chat interface."""
-
-    def get(self, request, channel_id, *args, **kwargs):
-        channel = get_object_or_404(Channel, id=channel_id)
-        messages = channel.messages.order_by("timestamp").all()
-        return render(
-            request, "chats/channel.html", {"channel": channel, "messages": messages}
-        )
+        channel.generate_invite_code()
+        context = {"channel": channel}
+        return render(request, "chats/partials/_invite_link.html", context)
