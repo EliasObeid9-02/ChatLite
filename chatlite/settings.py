cd85ebabc4a7c91e51891a252afd71a7f70847a7
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-30 19:03:06 +0300

    fix: added Render Deployment to allowed hosts

--- chatlite/settings.py
+++ chatlite/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = [
-    "https://chatlite-9a1y.onrender.com",
+    "chatlite-9a1y.onrender.com",
     ".localhost",
     "127.0.0.1",
 ]
