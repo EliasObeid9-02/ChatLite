3e1975e2b34939a7fcf4b5639c55dc18698496e2
Elias Obeid <elias.obeid9.02@gmail.com>
2025-09-21 17:56:21 +0300

    feat(ui): Added support for dynamically changing active channel in sidebar

--- chatlite/context_processors.py
+++ chatlite/context_processors.py
@@ -1,16 +1,7 @@
-import re
-import uuid
-
-
 def sidebar_context(request):
     context = {}
     if request.user.is_authenticated:
         context["channels"] = request.user.member_of.all()
-
-        # Extract active channel ID from the request path
-        match = re.search(r"channel/([0-9a-f-]+)/", request.path)
-        active_channel_id = uuid.UUID(match.group(1)) if match else None
-        context["active_channel_id"] = active_channel_id
     return context
 
 
